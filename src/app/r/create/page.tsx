0511acb8edd31cc61e333529c32610f048ad8c01
Alexander Yalcin <46671361+AlexanderYalcin@users.noreply.github.com>
2023-10-09 22:28:11 +0200

    Schema

---
+++ src/app/r/create/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { useState } from 'react';
+
+const Page = () => {
+  const [input, setInput] = useState<string>('');
+
+  return <div>Page</div>;
+};
+
+export default Page;
